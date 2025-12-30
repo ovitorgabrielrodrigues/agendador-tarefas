1c2db69b3e5c1486cdf601c14b69b8d9d11b9594
Vitor <vitor.gabriel22@edu.unifil.br>
2025-12-29 21:18:59 -0300

    commit para teste de aplicacao pela professora

---
+++ src/main/java/infrastructure/business/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package infrastructure.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+}
---
+++ src/main/java/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package infrastructure.security;
+
+
+import infrastructure.business.dto.UsuarioDTO;
+import infrastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+
+    private UsuarioClient client;
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return   User.withUsername(usuarioDTO.getEmail())
+                .password(usuarioDTO.getSenha())
+                .build();
+    }
+
+}
